8878f321e3be70c9129b5f9c3be566748e656de7
rose-theorem <theorem.roselyn@gmail.com>
2022-04-05 23:03:47 -0700

    commit on 'main'

---
+++ src/components/counter.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="h3 display-3">{{this.count}}</div>
+                        <button class="btn btn-success" @click="this.incr">Increment</button>
+                        <button class="btn btn-warning" @click="this.decr">Decrement</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CounterNumber',
+  data: function () {
+    return {
+      count: 88
+    }
+  },
+  methods: {
+    incr: function () {
+      this.count = this.count + 1
+    },
+    decr: function () {
+      this.count = this.count - 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
